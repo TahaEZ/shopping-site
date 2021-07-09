31f7f7b5b91cb05eb941c0778d9dc576aee36c8b
TahaEZ <taha13800111@gmail.com>
2021-07-09 13:39:36 +0430

    Update products.api.get.js

--- src/api/products.api.get.js
+++ src/api/products.api.get.js
@@ -1,11 +1,32 @@
 import { instance } from '../http'
 
-export function fetchProducts(resolve, reject, page, limit, field) {
+export function fetchProducts(
+	resolve,
+	reject,
+	page,
+	limit,
+	field,
+	sortBy,
+	order,
+	subfield,
+	id
+) {
 	let url = '/store-products'
 	if (page && limit && !field) url += `?_page=${page}&_limit=${limit}`
 	else if (field) url += `?field=${field}`
 	else if (page && limit && field)
 		url += `?field=${field}&_page=${page}&_limit=${limit}`
+	if (sortBy)
+		url +=
+			url == '/store-products'
+				? `?_sort=${sortBy}&_order=${order}`
+				: `&_sort=${sortBy}&_order=${order}`
+	if (subfield)
+		url +=
+			url == '/store-products'
+				? `?subfield=${subfield}`
+				: `&subfield=${subfield}`
+	if (id) url += url == '/store-products' ? `?id=${id}` : `&id=${id}`
 	console.log(instance)
 	return instance
 		.get(url)

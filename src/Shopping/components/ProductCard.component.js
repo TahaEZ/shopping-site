7cfe013975c42c23bd19fb1d919d7cee08eb86f9
TahaEZ <taha13800111@gmail.com>
2021-07-09 13:33:34 +0430

    Created Product Card Component

---
+++ src/Shopping/components/ProductCard.component.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+const ProductCard = (props) => {
+	const { image, price, name, id } = props.product
+	return (
+		<Link
+			to={`/product/${id}`}
+			style={{
+				display: 'block',
+				border: 'solid black 1px',
+				width: '28vw',
+				height: '25vh',
+				textAlign: 'center',
+				margin: '1%',
+				boxSizing: 'border-box',
+				color: 'black',
+				textDecoration: 'none',
+			}}
+		>
+			<figure
+				className='d-flex justify-content-around align-items-center py-1 px-2'
+				id={id}
+				style={{ width: '100%', height: '100%' }}
+			>
+				<img
+					src={process.env.PUBLIC_URL + `/images/${image}`}
+					style={{ width: '30%', height: '70%' }}
+				></img>
+				<figcaption
+					className='d-flex flex-column justify-content-center align-items-center'
+					style={{ width: '60%' }}
+				>
+					<h3 style={{ fontSize: '1.1rem' }}>{name}</h3>
+					<h4 style={{ fontSize: '.9rem' }}>{price} تومان</h4>
+				</figcaption>
+			</figure>
+		</Link>
+	)
+}
+export { ProductCard }
